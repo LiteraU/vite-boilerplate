cb1838c12a1b92e1e02e260f4d44dd1c2ff900b8
Artiom Climenko <artiom.climenko@yandex.ru>
2022-01-10 18:00:44 +0300

    feat(Main Window): Implement the main window

---
+++ src/components/MainWindow/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FilterAndSearch, Window } from './index.styles';
+
+export function MainWindow() {
+  return (
+    <Window>
+      <FilterAndSearch>
+
+      </FilterAndSearch>
+    </Window>
+  );
+}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Container } from './index.styles';
 import { PageTitle } from '../components/PageTitle';
 import { TabBar } from '../components/TabBar';
+import { MainWindow } from '../components/MainWindow';
 
 export function DashboardPage() {
   return (
     <Container>
       <PageTitle />
       <TabBar />
+      <MainWindow />
     </Container>
   );
 }
