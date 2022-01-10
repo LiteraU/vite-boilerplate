2e71bd5b7f39a6bcc2127e48ea8a641c056de9be
Artiom Climenko <artiom.climenko@yandex.ru>
2022-01-10 18:18:37 +0300

    feat(Search Field): Implement a search field

--- src/components/MainWindow/index.tsx
+++ src/components/MainWindow/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { FilterAndSearch, Window } from './index.styles';
 import { FilterButton } from '../FilterButton';
+import { SearchField } from '../SearchField';
 
 export function MainWindow() {
   return (
     <Window>
       <FilterAndSearch>
         <FilterButton />
+        <SearchField />
       </FilterAndSearch>
     </Window>
   );
