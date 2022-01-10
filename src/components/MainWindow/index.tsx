880f2f28c507289e3585b30c32d6307c10fb58b3
Artiom Climenko <artiom.climenko@yandex.ru>
2022-01-10 18:08:32 +0300

    feat(Filter Button): Implement a filter button

--- src/components/MainWindow/index.tsx
+++ src/components/MainWindow/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { FilterAndSearch, Window } from './index.styles';
+import { FilterButton } from '../FilterButton';
 
 export function MainWindow() {
   return (
     <Window>
       <FilterAndSearch>
-
+        <FilterButton />
       </FilterAndSearch>
     </Window>
   );
